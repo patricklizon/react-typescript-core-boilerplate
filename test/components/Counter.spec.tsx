919e123210b600d9edfbbc859d5057c39a1b5eb3
Patryk Lizoń <lizon.patryk@gmail.com>
2021-03-05 01:33:12 +0100

    refactor: Improve Counter test

--- test/components/Counter.spec.tsx
+++ test/components/Counter.spec.tsx
@@ -6,26 +6,45 @@ import fire from "@testing-library/user-event";
 import { Counter } from "../../src/components/Counter";
 
 describe("Counter", () => {
-  const c = render(<Counter data-testid="c" />);
-  const display = () => c.getByTestId("c.display");
-
-  it("renders", () => {
-    expect(display().textContent).toEqual("0");
-  });
-
   it("increments after clicking on increment button", () => {
     const c = render(<Counter data-testid="c" />);
     const display = () => c.getByTestId("c.display");
-    const incBtn = () => c.getByTestId("c.actions.increment");
+    const incrementBtn = () => c.getByTestId("c.actions.increment");
+    const decrementBtn = () => c.getByTestId("c.actions.decrement");
 
     expect(display().textContent).toEqual("0");
 
-    fire.click(incBtn());
+    fire.click(incrementBtn());
 
     expect(display().textContent).toEqual("1");
 
-    fire.click(incBtn());
+    fire.click(incrementBtn());
 
     expect(display().textContent).toEqual("2");
+
+    fire.click(decrementBtn());
+
+    expect(display().textContent).toEqual("1");
+
+    fire.click(decrementBtn());
+
+    expect(display().textContent).toEqual("0");
+  });
+
+  it("increments and decrements with custom step", () => {
+    const c = render(<Counter data-testid="c" step={5} />);
+    const display = () => c.getByTestId("c.display");
+    const incrementBtn = () => c.getByTestId("c.actions.increment");
+    const decrementBtn = () => c.getByTestId("c.actions.decrement");
+
+    expect(display().textContent).toEqual("0");
+
+    fire.click(incrementBtn());
+
+    expect(display().textContent).toEqual("5");
+
+    fire.click(decrementBtn());
+
+    expect(display().textContent).toEqual("0");
   });
 });
