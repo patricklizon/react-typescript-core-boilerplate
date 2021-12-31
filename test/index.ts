2341b68301467a64d74816971d7ff28348fd8e8a
Patryk Lizoń <lizon.patryk@gmail.com>
2021-12-31 15:38:19 +0100

    chore: Update testing setup

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,11 @@
 import fireEvent from "@testing-library/user-event";
+import { expect, use } from "chai";
+import chaiDom from "chai-dom";
+import sinon from "sinon";
+import chaiSinon from "sinon-chai";
 export * as tl from "@testing-library/react";
-export { expect } from "chai";
-export { fireEvent };
+
+use(chaiDom);
+use(chaiSinon);
+
+export { expect, fireEvent, sinon };
