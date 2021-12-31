a8dc243599a156789343089ba6e7ce723f7c5378
Patryk Lizoń <lizon.patryk@gmail.com>
2021-12-31 14:38:38 +0100

    chore: Update eslint config

--- src/@types/files.d.ts
+++ src/@types/files.d.ts
@@ -1,2 +1,11 @@
-declare module "*.svg";
-declare module "*.css";
+declare module "*.svg" {
+  const content: string;
+  // eslint-disable-next-line import/no-default-export
+  export default content;
+}
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  // eslint-disable-next-line import/no-default-export
+  export default classes;
+}
