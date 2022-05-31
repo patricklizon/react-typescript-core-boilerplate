79f75d1bcafc49be0831bd22b93357cfdb2352bf
Patryk Lizoń <lizon.patryk@gmail.com>
2022-05-31 18:01:26 +0200

    chore: Remove sinon

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,7 @@
 import userEvent from "@testing-library/user-event";
 import { expect, use } from "chai";
 import chaiDom from "chai-dom";
-import chaiSinon from "sinon-chai";
 
 use(chaiDom);
-use(chaiSinon);
 
 export { expect, userEvent };
